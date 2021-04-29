15d5bc0931770c2e0e484e6c7ab45d0b121c76f5
OldMan35 <kostya_kv@mail.ru>
2021-04-30 00:08:45 +0300

    add dependency

---
+++ src/main/java/ru/currencyconverter/Controller.java
@@ -0,0 +1,166 @@
+package ru.currencyconverter;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+
+public class Controller extends ParserJson {
+    private String firstValue;
+    private String secondValue;
+    private double valueController;
+    private String valuteNameController;
+    private long valuteNominalController;
+    private double secondValueValuteController;
+    private String secondNameValuteController;
+    private long secondNominalValuteController;
+
+    public void conversionCurrency() throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter the currency to convert: ");
+
+        firstValue = reader.readLine(); // ввод данных. получаем данные 1 валюты (вид валюты) от пользователя
+        setCurrency(firstValue);//устанавливаем вид валюты для конвертации
+        parser();
+        valueController = (double) getValuteValue();//значение валюты
+        valuteNameController = (String) getValuteName();//название валюты
+        valuteNominalController = (long) getValuteNominal();//номинал валюты
+        System.out.println("Nominal : " + valuteNominalController + " " + valuteNameController);
+        System.out.println(valueController + " " + " rub.");
+
+        secondValue = reader.readLine(); // получаем данные 2 валюты (вид валюты) от пользователя
+        setCurrency(secondValue);//устанавливаем вид валюты для конвертации
+        parser();
+        secondValueValuteController = (double) getValuteValue();
+        secondNameValuteController = (String) getValuteName();
+        secondNominalValuteController = (long) getValuteNominal();
+        System.out.println("Nominal : " + secondNominalValuteController + " " + secondNameValuteController);
+        System.out.println(secondValueValuteController + " " + " rub.");
+
+        //вычисляем значние курса по отношению одной валюты к другой
+        //1. если наминалы обеих валют равны 1
+        if (valuteNominalController == 1 && secondNominalValuteController == 1) {
+            double resultNominal = valueController / secondValueValuteController;
+            System.out.println("In one " + valuteNameController + " " + resultFormat(resultNominal) + " " + secondNameValuteController);
+            System.out.println("end");
+
+            //2. если наминал обеих валют > 1, приводим к значению 1
+        } else if (valuteNominalController > 1 && secondNominalValuteController > 1) {
+            double resultNominal = (valueController / valuteNominalController) / (secondValueValuteController / secondNominalValuteController);
+            System.out.println("In one " + valuteNameController + " " + resultFormat(resultNominal) + " " + secondNameValuteController);
+            System.out.println("end");
+
+            //3. если номинал 1 валюты больше 1
+        } else if (valuteNominalController > 1 && secondNominalValuteController == 1) {
+            double resultNominal = (valueController / valuteNominalController) / secondValueValuteController;
+            System.out.println("In one " + valuteNameController + " " + resultFormat(resultNominal) + " " + secondNameValuteController);
+            System.out.println("end");
+
+            //4. если номинал 2 валюты больше 1
+        } else if (valuteNominalController == 1 && secondNominalValuteController > 1) {
+            double resultNominal = valueController / (secondValueValuteController / secondNominalValuteController);
+            System.out.println("In one " + valuteNameController + " " + resultFormat(resultNominal) + " " + secondNameValuteController);
+            System.out.println("end");
+
+            //если что то пошло не так))
+        } else {
+            System.out.println("Something's wrong!");
+        }
+
+    }
+
+    //formatted  double resultNominal
+    public String resultFormat(double result) {
+        String resultFormatted = String.format("%.5f", result);
+        return resultFormatted;
+    }
+
+
+    public String getFirstValue() {
+
+        return firstValue;
+    }
+
+    public void setFirstValue(String firstValue) {
+
+        this.firstValue = firstValue;
+    }
+
+    public double getValueController() {
+
+        return valueController;
+    }
+
+    public void setValueController(double valueController) {
+
+        this.valueController = valueController;
+    }
+
+    public String getValuteNameController() {
+
+        return valuteNameController;
+    }
+
+    public void setValuteNameController(String valuteNameController) {
+        this.valuteNameController = valuteNameController;
+    }
+
+    public void setValuteNominalController(int valuteNominalController) {
+        this.valuteNominalController = valuteNominalController;
+    }
+
+    public double getSecondValueValuteController() {
+
+        return secondValueValuteController;
+    }
+
+    public void setSecondValueValuteController(double secondValueValuteController) {
+        this.secondValueValuteController = secondValueValuteController;
+    }
+
+    public String getSecondNameValuteController() {
+
+        return secondNameValuteController;
+    }
+
+    public void setSecondNameValuteController(String secondNameValuteController) {
+        this.secondNameValuteController = secondNameValuteController;
+    }
+
+    public long getSecondNominalValuteController() {
+
+        return secondNominalValuteController;
+    }
+
+    public void setSecondNominalValuteController(long secondNominalValuteController) {
+        this.secondNominalValuteController = secondNominalValuteController;
+    }
+
+    public String getFirsValue() {
+        return firstValue;
+    }
+
+    public void setFirsValue(String firsValue) {
+
+        this.firstValue = firsValue;
+    }
+
+    public String getSecondValue() {
+
+        return secondValue;
+    }
+
+    public void setSecondValue(String secondValue) {
+
+        this.secondValue = secondValue;
+    }
+
+    public long secondNominalValuteController() {
+
+        return secondNominalValuteController;
+    }
+
+    public long getValuteNominalController() {
+
+        return valuteNominalController;
+    }
+}
--- src/main/java/ru/currencyconverter/ParserJson.java
+++ src/main/java/ru/currencyconverter/ParserJson.java
@@ -1,17 +1,16 @@
 package ru.currencyconverter;
 
+
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.io.InputStream;
 import java.net.URL;
-import java.util.Date;
 
-@RestController
-public class DefaultController {
-    private String currency = "AUD";
+
+public class ParserJson {
+
+    private String currency;
     private final String name = "Name";
     private final String value = "Value";
     private final String valute = "Valute";
@@ -20,8 +19,7 @@ public class DefaultController {
     private Object valuteName;
     private Object valuteValue;
 
-    @RequestMapping("/volute")
-    public Object parser() throws Exception {
+    public void parser() throws Exception {
 
         URL url = new URL("https://www.cbr-xml-daily.ru/daily_json.js");//Создает объект URL с путем к странице
         InputStream input = url.openStream();//Получает InputStream у интернет-объекта
@@ -35,12 +33,25 @@ public class DefaultController {
         valuteNominal = valuteСontent.get(nominal);//получаем номинал валюты
         valuteName = valuteСontent.get(name);//название валюты для вывода пользователю
         valuteValue = valuteСontent.get(value);//значение валюты для расчета значения конвертации
-        return valuteName + " " + valuteValue;
     }
 
-    @RequestMapping("/index")
-    public String index() {
+    public void setCurrency(String currency) {
 
-        return (new Date().toString());
+        this.currency = currency;
+    }
+
+    public Object getValuteName() {
+
+        return valuteName;
+    }
+
+    public Object getValuteValue() {
+
+        return valuteValue;
+    }
+
+    public Object getValuteNominal() {
+
+        return valuteNominal;
     }
 }
