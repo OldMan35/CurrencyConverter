201f751abf47b3957c5c967d2675e66190687951
OldMan35 <kostya_kv@mail.ru>
2021-05-03 18:07:22 +0300

    add branch

--- src/main/java/ru/currencyconverter/controller/DefaultController.java
+++ src/main/java/ru/currencyconverter/controller/DefaultController.java
@@ -11,31 +11,5 @@ import java.net.URL;
 
 @Controller
 public class DefaultController {
-    private String currency = "AUD";
-    private final String name = "Name";
-    private final String value = "Value";
-    private final String valute = "Valute";
-    private final String nominal = "Nominal";
-    private Object valuteNominal;
-    private Object valuteName;
-    private Object valuteValue;
 
-
-    @GetMapping("/currency")
-    public Object parser() throws Exception {
-
-        URL url = new URL("https://www.cbr-xml-daily.ru/daily_json.js");//Создает объект URL с путем к странице
-        InputStream input = url.openStream();//Получает InputStream у интернет-объекта
-        byte[] buffer = input.readAllBytes();//Читает все байты и возвращает массив байт
-        String strings = new String(buffer);//Преобразуем массив в строку
-        JSONParser parser = new JSONParser();
-        Object obj = parser.parse(strings);// парсим строку
-        JSONObject jsonObject = (JSONObject) obj;//получаем объект JSON
-        JSONObject jsonСontent = (JSONObject) jsonObject.get(valute);//разбираем объект по значениям
-        JSONObject valuteСontent = (JSONObject) jsonСontent.get(currency);//получаем запрос на вид валюты от пользователя
-        valuteNominal = valuteСontent.get(nominal);//получаем номинал валюты
-        valuteName = valuteСontent.get(name);//название валюты для вывода пользователю
-        valuteValue = valuteСontent.get(value);//значение валюты для расчета значения конвертации
-        return valuteName + " " + valuteValue;
-    }
 }
