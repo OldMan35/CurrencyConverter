308a8790c809819a8236fad7903103eee5e74864
OldMan35 <kostya_kv@mail.ru>
2021-05-15 12:27:03 +0300

    change index.html+DefaultController

--- src/main/java/ru/currencyconverter/controller/DefaultController.java
+++ src/main/java/ru/currencyconverter/controller/DefaultController.java
@@ -4,12 +4,22 @@ package ru.currencyconverter.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
 public class DefaultController {
     @GetMapping("/")
     public String homePage() {
+
         return "index";
+    }
+
+    @PostMapping()
+    public String convert(@RequestParam("fromAmount") String fromAmount, @RequestParam("toAmount") String toAmount) {
+
+        System.out.println(fromAmount + toAmount);
+        return "redirect:/";
     }
 }
