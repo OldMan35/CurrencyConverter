e7828bac6a489debc5a9dedc09fd0f638b8dce91
OldMan35 <kostya_kv@mail.ru>
2021-05-21 17:20:32 +0300

    working code

--- src/main/java/ru/currencyconverter/controller/DefaultController.java
+++ src/main/java/ru/currencyconverter/controller/DefaultController.java
@@ -9,20 +9,20 @@ import ru.currencyconverter.model.Valute;
 
 @Controller
 public class DefaultController {
-
+    //done
     @GetMapping("/home_page")
     public String homePage() {
 
         return "home_page";
     }
-
+    //done
     @GetMapping("/conversion_form")
     public String showForm(Model model) {
         Valute valute = new Valute();
         model.addAttribute("valute", valute);
         return "/conversion_form";
     }
-
+    //done
     @PostMapping("/conversion_form")
     @RequestMapping(value = "/conversion_form", method = RequestMethod.POST)
     public String convert(@ModelAttribute("valute") Valute valute) {
@@ -30,6 +30,7 @@ public class DefaultController {
         return "/result";
     }
 
+    //done
     @GetMapping("/result")
     public String resultConvert() {
         return "result";
