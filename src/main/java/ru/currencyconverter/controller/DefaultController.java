c65cd46ed924262e1d97fa1be7b258c2d35328e3
OldMan35 <kostya_kv@mail.ru>
2021-05-24 23:37:02 +0300

    DefaultController

--- src/main/java/ru/currencyconverter/controller/DefaultController.java
+++ src/main/java/ru/currencyconverter/controller/DefaultController.java
@@ -35,7 +35,6 @@ public class DefaultController {
     }
 
     @PostMapping("/conversion_form")
-
     public String convert(@ModelAttribute("valute") Valute valute) {
         valuteDAO.getVolume(valute);
         return "redirect:/result";
